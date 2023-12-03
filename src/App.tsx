a3d5f11805ca27fee8c9cd92b92ed8aa6c0a1967
Prashant <prashantco111@gmail.com>
2023-12-03 15:03:01 +0530

    Refactor component imports and add Main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import Example from '@/pages/example'
-import NavBar from './components/NavBar'
+import NavBar from '@/components/NavBar'
+import Main from '@/pages/Main'
 
 function App() {
 	return (
 		<div class="flex min-h-screen w-full flex-col bg-gray-200 dark:bg-slate-800">
 			<NavBar />
-			<Example />
+			<Main />
 		</div>
 	)
 }
