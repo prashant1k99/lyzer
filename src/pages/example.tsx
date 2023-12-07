41eecc79b40835d2c8237430a5ad5beb93686b9e
Prashant <prashantco111@gmail.com>
2023-12-07 13:00:44 +0530

    Migrated to React from SolidJS

--- src/pages/example.tsx
+++
@@ -1,7 +0,0 @@
-export default function Example() {
-	return (
-		<div class="mx-auto w-full grow lg:flex xl:px-2">
-			{/* Left sidebar & main wrapper */}
-		</div>
-	)
-}
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
-import path from "path"
-import solid from 'vite-plugin-solid'
-import devtools from 'solid-devtools/vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [devtools({
-    /* features options - all disabled by default */
-    autoname: true, // e.g. enable autoname
-  }), solid()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [react()],
 })
